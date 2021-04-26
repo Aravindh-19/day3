869c9a065860eb4bf667d36d320678f6beae9112
Aravindh <aravindhbme@gmail.com>
2021-04-26 15:08:59 +0530

    first commit

---
+++ ConsoleApp13/ConsoleApp13/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Hi i am singh";
+            var unique = new HashSet<char>(s);
+            Console.WriteLine("Original string:"+s);
+            Console.Write("Corrected string:");
+            foreach (char c in unique)
+            Console.Write( c);
+            Console.ReadLine();
+        }
+    }
+}
