963a8a4e067b3e1460e9ee0d270b0d415c7ffa01
Aravindh <aravindhbme@gmail.com>
2021-04-26 14:44:58 +0530

    first commit

---
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string s = "It is a bad color";
+            
+            if(s.Contains("is a"))
+            {
+                Console.WriteLine("Yes!string present");
+            }
+            else
+            {
+                Console.WriteLine("No!string not present");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "My name is Aravindh";
+            char ch;
+            Console.WriteLine("String before converstion:" + s);
+            System.Text.StringBuilder str = new System.Text.StringBuilder();
+            for (int i = 0; i < s.Length; i++)
+                if (char.IsLower(s[i]))
+                {
+                    ch = Char.ToUpper(s[i]);
+                    str.Append(ch);
+                }
+                else if (char.IsUpper(s[i]))
+                {
+                    ch = Char.ToLower(s[i]);
+                    str.Append(ch);
+                }
+                else
+                {
+                    ch = s[i];
+                    str.Append(ch);
+                }
+            Console.WriteLine("String after converstion:" + str);
+            Console.ReadLine();
+        }
+        
+    }
+  
+}
